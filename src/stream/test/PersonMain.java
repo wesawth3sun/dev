bf4b51582144446a0718972dae84199b815078b1
SUN <suunn001@gmail.com>
2025-01-03 12:17:57 +0900

    목록 생성

--- src/stream/test/PersonMain.java
+++ src/stream/test/PersonMain.java
@@ -1,6 +1,7 @@
 package stream.test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalDouble;
 
--- src/wiseSaying/App.java
+++ src/wiseSaying/App.java
@@ -6,20 +6,30 @@ public class App {
     public void run() {
         Scanner scanner = new Scanner(System.in);
         int lastNo = 1;
+        String content = "";
+        String author = "";
+
         System.out.println("== 명언 앱 ==");
         while (true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
-            if (command.equals("등록")) {
-                System.out.print("명언: ");
-                String wiseSaying = scanner.nextLine();
-                System.out.print("작가: ");
-                String author = scanner.nextLine();
-                System.out.println(lastNo + " 번 명업이 등록되었습니다.");
-                lastNo++;
-            } else if (command.equals("종료")) {
+
+            if (command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언: ");
+                content = scanner.nextLine();
+
+                System.out.print("작가: ");
+                author = scanner.nextLine();
+
+                System.out.println(lastNo + " 번 명업이 등록되었습니다.");
+                lastNo++;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-------------------");
+                System.out.println(lastNo + " / " + author + " / " + content);
             }
         }
     }
