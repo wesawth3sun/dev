fffc3246363de3f5063c4d3c2144a71e4fccec0b
SUN <suunn001@gmail.com>
2025-01-06 17:58:39 +0900

    테스트봇 생성 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("등록\n즐길 수 없다면 피해라\n작자 미상");
+
+        String string1 = scanner.nextLine();
+        System.out.println(string1);
+
+        String string2 = scanner.nextLine();
+        System.out.println(string2);
+
+        String string3 = scanner.nextLine();
+        System.out.println(string3);
+    }
+
+
+}
